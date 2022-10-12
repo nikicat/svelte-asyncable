3b5e68054dbf6d1fe3568df2d421e4460b7c7072
Nikolay Bryskin <nbryskin@gmail.com>
2022-10-12 20:21:46 +0300

    Set initial store value to null to avoid duplicate Promise and "Uncaught (in promise)"

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import { writable, derived, get } from 'svelte/store';
 
 export function asyncable(getter, setter = () => {}, stores = []) {
-	let resolve;
-	const initial = new Promise((res) => (resolve = res));
-
 	const derived$ = derived(stores, (values) => values);
 
-	const store$ = writable(initial, (set) => {
+	const store$ = writable(null, (set) => {
 		return derived$.subscribe(async (values = []) => {
 			let value = getter(...values);
 			if (value === undefined) return;
 			value = Promise.resolve(value);
 			set(value);
-			resolve(value);
 		});
 	});
 
